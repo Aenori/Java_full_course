2fedca9544654364ee526fc4026771f446797464
Nicolas Rousset <nrousset@gmail.com>
2017-05-27 14:59:08 +0200

    construct_and_dispatch is working

--- lectures/construct_and_dispatch_all.py
+++ lectures/construct_and_dispatch_all.py
@@ -1,8 +1,18 @@
-import os
+import os, shutil
 import subprocess
+
+
 for dirpath, dirnames, filenames in os.walk("src"):
     for it_file in filter( lambda x : x.endswith(".txt"), filenames ):
-        subprocess.check_call( ["/home/nicolas/dev/tools/usr_bin/make_a4_htmlpdf.sh", it_file ] )
+
+        call_parameters = ["/bin/bash", "/home/nicolas/dev/tools/usr_bin/make_a4_htmlpdf.sh", dirpath + "/" + it_file ]
+        pdf_path, pdf_file_name   = "pdf/"  + dirpath[4:], it_file[:-4] + ".pdf"
+        html_path, html_file_name = "html/" + dirpath[4:], it_file[:-4] + ".html"
+        print( call_parameters )
+        subprocess.check_call( call_parameters )
+        
+        shutil.move( "pdf/"  + pdf_file_name, pdf_path  + "/" + pdf_file_name )
+        shutil.move( "pdf/" + html_file_name, html_path + "/" + html_file_name )
 
 
 
